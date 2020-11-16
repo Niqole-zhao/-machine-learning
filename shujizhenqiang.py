f1af1cbc9853c73520cedd3ff0da2bf399b97555
Niqole-zhao <70828769+Niqole-zhao@users.noreply.github.com>
2020-11-16 19:42:07 +0800

    Add files via upload

---
+++ shujizhenqiang.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+fashion = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)   #让数据是4维的，和网络匹配
+
+image_gen_train = ImageDataGenerator(
+    rescale=1. / 1.,   #图像灰度值为255时候，归至0~1
+    rotation_range=45,
+    width_shift_range=.15,
+    height_shift_range=.15,
+    horizontal_flip=True,
+    zoom_range=0.5
+)
+image_gen_train.fit(x_train)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer="adam",
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metries=["sparse_categorical_accuracy"])
+model.fit(image_gen_train.flow(x_train, y_train, batch_size=32), epochs=5, validation_data=(x_test, y_test),
+          validation_freq=1)
+model.summary()
